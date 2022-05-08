0ae8928c7030d2764d4d85ab2f08092e91fe225a
lucascardosoc <lucas.cardosopessoal@hotmail.com>
2022-05-07 21:32:30 -0300

    exercicio18

--- exercicio18.py
+++ exercicio18.py
@@ -1,1 +1,6 @@
 alturaEscada = 2
+alturaAlcancar = int(input("Qual altura em metros deseja alncancar subindo a escada? "))
+
+objetivo = alturaAlcancar / alturaEscada
+
+print("Ele deverá subir", objetivo, "degraus")
